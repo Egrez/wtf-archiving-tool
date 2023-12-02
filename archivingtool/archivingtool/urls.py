916371208fa6e15c3edf6bb73e655965dd0f8ab0
serge <sergealecrivera@gmail.com>
2023-12-02 22:07:46 +0800

    added urls for oauth and callback

--- archivingtool/archivingtool/urls.py
+++ archivingtool/archivingtool/urls.py
@@ -22,4 +22,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('token/', views.generate_token, name='token'),
+    path('oauthcallback/', views.callback, name="oauth"),
 ]
