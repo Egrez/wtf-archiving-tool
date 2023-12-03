313f50304cca8b4f23c8d428227f4d91ec44e183
serge <sergealecrivera@gmail.com>
2023-12-03 18:11:55 +0800

    added class to URL input

--- archivingtool/archivingtool/forms.py
+++ archivingtool/archivingtool/forms.py
@@ -2,11 +2,8 @@ from django import forms
 
 class PostUrlForm(forms.Form):  
 	url = forms.CharField(
-		label='Post URL',
-		# widget = forms.TextInput(attrs = {
-		# 	"class" : "text",
-        #     'style' : 'width: 500px; height: 80px; text-align: center; border-radius: 40px; box-shadow: 4px 6px 5px 0 rgba(0, 0, 0, 0.3); margin-top: 150px; margin-left: 200px; font-family: Poppins_bold; font-size: 40px;',
-        #     'placeholder' : 'Event Name',
-		# 	"name" : "fname",
-		# }),
+		widget = forms.TextInput(attrs = {
+			"class" : "user-input",
+            "placeholder" : "Enter disinformation post URL here",
+		}),
 	)
